eabfd6be7292d778a532bfe6fdedee271be1e913
Renziera <stanleyheryanto@gmail.com>
2019-11-12 12:53:45 +0000

    Vue & Firebase configs

--- joints/src/main.js
+++ joints/src/main.js
@@ -1,12 +1,28 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/storage';
 
 Vue.config.productionTip = false;
 
+const firebaseConfig = {
+    apiKey: 'AIzaSyAvwS9hac8KtPC-5qt3v5c9rQIadcu30jw',
+    authDomain: 'joints-ugm.firebaseapp.com',
+    databaseURL: 'https://joints-ugm.firebaseio.com',
+    projectId: 'joints-ugm',
+    storageBucket: 'joints-ugm.appspot.com',
+    messagingSenderId: '382054057518',
+    appId: '1:382054057518:web:571099e5c44e83ed40c1bf'
+};
+
+firebase.initializeApp(firebaseConfig);
+
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#app');
---
+++ joints/vue.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    devServer: {
+        public: 'kimac.joints.id',
+        https: true
+    }
+};
